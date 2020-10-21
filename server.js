88f2b4ca17859fd9e4167c4ca9a4f3bddd0c8a18
Slogreco <shoshwright@hotmail.com>
2020-10-20 19:49:22 -0700

    set up routes

--- server.js
+++ server.js
@@ -7,7 +7,22 @@ const PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//====================================================
+const reservations = [];
+const waitlist = [];
 
-app.listen(PORT, function() {
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/home.html"));
+});
+
+app.get("/reservations", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/reserve.html"));
+});
+
+app.get("/tables", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/table.html"));
+});
+
+app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
 });
