f1ee6f4b606c341102620481597759eefdb45574
Slogreco <shoshwright@hotmail.com>
2020-10-20 20:30:50 -0700

    created api calls

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //====================================================
-const reservations = [];
+const tables = [];
 const waitlist = [];
 
 app.get("/", function (req, res) {
@@ -21,6 +21,29 @@ app.get("/reservations", function (req, res) {
 
 app.get("/tables", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/table.html"));
+});
+
+app.get("/api/tables", function (req, res) {
+    return res.json(tables);
+});
+
+app.get("/api/waitlist", function (req, res) {
+    return res.json(waitlist);
+});
+
+app.post("/api/reservation", function (req, res) {
+    const newReservation = req.body;
+
+    console.log(newReservation);
+if (tables.length < 5 ){
+    tables.push(newReservation);
+    res.json(true);
+}
+else {
+    waitlist.push(newReservation);
+    res.json(false)
+}
+    
 });
 
 app.listen(PORT, function () {
